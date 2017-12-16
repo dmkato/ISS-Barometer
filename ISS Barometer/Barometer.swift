7f2c0b8dd2ddbf06b4e5a4ddd8c2512789065acd
dmkato <dmkato@gmail.com>
2017-12-15 19:33:56 -0800

    Created a Barometer class that is instantiated in the main view controller

--- ISS Barometer/Barometer.swift
+++ ISS Barometer/Barometer.swift
@@ -2,7 +2,7 @@
 //  Barometer.swift
 //  ISS Barometer
 //
-//  Created by Daniel Kato on 12/14/17.
+//  Created by Daniel Kato on 12/15/17.
 //  Copyright © 2017 ISSBarometer. All rights reserved.
 //
 
@@ -16,68 +16,52 @@ class Barometer {
     var deltaMmHg:Double? = 0.0
     var prevTime:Double? = 0.0
     var time:Double? = 0.0
-    var significantDigits:Int = 4
-    var prevDebugData:Double? = 760.00
-    var debugData:Double? = 0.0
+    
+    // REMOVE BEFORE DEPLOY <--
+    var prevDebugData:Double? = 0.0
+    var debugData:Double? = 10.0
     var deltaDebug:Double? = 0.0
+    // -->
     
     func kPa2mmHg(kPa:Double) -> Double {
-        let atm:Double = kPa * 101.325
-        let mmHg:Double = atm / 760.0
-        return mmHg
+        return kPa * 7.50061683
     }
     
-    func handlePressureReading(data:CMAltitudeData) {
-        let kPa = data.pressure.doubleValue
-        prevTime = time
-        time = data.timestamp
-        prevMmHg = mmHg
-        mmHg = kPa2mmHg(kPa: kPa)
-        deltaMmHg = (mmHg! - prevMmHg!) / (time! - prevTime!)
-        
-        // Set Pressure Readings
-        let fString = "%.\(significantDigits)f mmHg"
-        pressureDisplay.text = String(format:fString, (mmHg)!)
-        deltaPressureDisplay.text = String(format:fString, (deltaMmHg)!)
-        
-        // Update Chart
-        chartViewController.updateChart(pressureReading: mmHg!,
-                                        time: time!)
-    }
-    
-    func startDisplayingPressureData() {
-        altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main,
-                                               withHandler: { data, error in
-            self.handlePressureReading(data: data!)
+    func startDisplayingPressureData(updateFunc:@escaping (Double, Double, Double) -> ()) {
+        altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main, withHandler: {
+            data, error in
+            let kPa = data?.pressure.doubleValue
+            self.prevTime = self.time
+            self.time = data?.timestamp
+            self.prevMmHg = self.mmHg
+            self.mmHg = self.kPa2mmHg(kPa: kPa!)
+            self.deltaMmHg = (self.mmHg! - self.prevMmHg!) / (self.time! - self.prevTime!)
+            updateFunc(self.mmHg!, self.deltaMmHg!, self.time!)
         })
     }
     
-    func start() {
-        if CMAltimeter.isRelativeAltitudeAvailable() {
-            startDisplayingPressureData()
-        } else {
-            startDisplayingDebugData()
+    // REMOVE BEFORE DEPLOY
+    func startDisplayingDebugData(updateFunc:@escaping (Double, Double, Double) -> ()) {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
+            _ in
+            self.prevTime = self.time
+            self.time = NSDate().timeIntervalSince1970
+            if (arc4random() % 2 == 0 || self.debugData! < 1){
+                self.debugData = self.debugData! + drand48()
+            } else {
+                self.debugData = self.debugData! - drand48()
+            }
+            self.deltaDebug = (self.debugData! - self.prevDebugData!) / (self.time! - self.prevTime!)
+            self.prevDebugData = self.debugData!
+            updateFunc(self.debugData!, self.deltaDebug!, self.time!)
         }
     }
     
-    //Debug Function to populate graph with random data, when barometer is unavalailable
-    func startDisplayingDebugData() {
-        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.displayDebugData), userInfo: nil, repeats: true)
-        //(timeInterval: 1, target: self, selector: #selector(MainViewController.displayDebugData), userInfo: nil, repeats: true)
-        
-    }
-    
-    @objc func displayDebugData() {
-        print("hello")
-        prevTime = time
-        time = NSDate().timeIntervalSince1970
-        debugData = debugData! - drand48()
-        deltaDebug = (debugData! - prevDebugData!) / (time! - prevTime!)
-        prevDebugData = debugData!
-        let fString = "%.\(significantDigits)f mmHg"
-        pressureDisplay.text = String(format:fString, (debugData)!)
-        deltaPressureDisplay.text = String(format:fString, (deltaDebug)!)
-        chartViewController.updateChart(pressureReading: debugData!,
-                                        time: time!)
+    func startBarometerUpdates(updateFunc:@escaping (Double, Double, Double) -> ()) {
+        if CMAltimeter.isRelativeAltitudeAvailable() {
+            startDisplayingPressureData(updateFunc: updateFunc)
+        } else {
+            startDisplayingDebugData(updateFunc: updateFunc)
+        }
     }
 }
--- ISS Barometer/MainViewController.swift
+++ ISS Barometer/MainViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreMotion
 import Charts
 
 class MainViewController: UIViewController {
@@ -15,90 +14,32 @@ class MainViewController: UIViewController {
     @IBOutlet weak var deltaPressureDisplay: UILabel!
     @IBOutlet weak var chartView: UIView!
     
-    lazy var altimeter :CMAltimeter = CMAltimeter()
-    var mmHg:Double? = 0.0
-    var prevMmHg:Double? = 0.0
-    var deltaMmHg:Double? = 0.0
-    var prevTime:Double? = 0.0
-    var time:Double? = 0.0
+    var barometer = Barometer()
     var significantDigits:Int = 4
-    
-    // REMOVE BEFORE DEPLOY <--
-    var prevDebugData:Double? = 0.0
-    var debugData:Double? = 10.0
-    var deltaDebug:Double? = 0.0
-    // -->
 
     lazy var chartViewController: ChartViewController = {
         return childViewControllers[0] as! ChartViewController
     }()
     
-    func kPa2mmHg(kPa:Double) -> Double {
-        return kPa * 7.50061683
-    }
-    
-    func handlePressureReading(data:CMAltitudeData) {
-        let kPa = data.pressure.doubleValue
-        prevTime = time
-        time = data.timestamp
-        prevMmHg = mmHg
-        mmHg = kPa2mmHg(kPa: kPa)
-        deltaMmHg = (mmHg! - prevMmHg!) / (time! - prevTime!)
-        
+    func updateUI(mmHg:Double, deltaMmHg:Double, time:Double) {
         // Set Pressure Readings
         let fString = "%.\(significantDigits)f mmHg"
-        pressureDisplay.text = String(format:fString, (mmHg)!)
-        deltaPressureDisplay.text = String(format:fString, (deltaMmHg)!)
-        
+        pressureDisplay.text = String(format:fString, mmHg)
+        deltaPressureDisplay.text = String(format:fString, deltaMmHg)
+
         // Update Chart
-        chartViewController.updateChart(pressureReading: mmHg!,
-                                        time: time!)
+        chartViewController.updateChart(pressureReading: mmHg,
+                                        time: time)
     }
     
-    func startDisplayingPressureData() {
-        altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main,
-                                               withHandler: { data, error in
-            self.handlePressureReading(data: data!)
-        })
-    }
-    
-    //Debug Function to populate graph with random data, when barometer is unavalailable
-    func startDisplayingDebugData() {
-        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.displayDebugData), userInfo: nil, repeats: true)
-    }
-    
-    // REMOVE BEFORE DEPLOY
-    @objc func displayDebugData() {
-        prevTime = time
-        time = NSDate().timeIntervalSince1970
-        if (arc4random() % 2 == 0 || debugData! < 1){
-            debugData = debugData! + drand48()
-        } else {
-            debugData = debugData! - drand48()
-        }
-        deltaDebug = (debugData! - prevDebugData!) / (time! - prevTime!)
-        prevDebugData = debugData!
-        let fString = "%.\(significantDigits)f mmHg"
-        pressureDisplay.text = String(format:fString, (debugData)!)
-        deltaPressureDisplay.text = String(format:fString, (deltaDebug)!)
-        chartViewController.updateChart(pressureReading: debugData!,
-                                        time: time!)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        if CMAltimeter.isRelativeAltitudeAvailable() {
-            startDisplayingPressureData()
-        // REMOVE BEFORE DEPLOY
-        } else {
-            startDisplayingDebugData()
-        }
+        barometer.startBarometerUpdates(updateFunc: updateUI)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
 
